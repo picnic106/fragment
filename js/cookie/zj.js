a127012cc8a9b7a03aa90064755ea898d4645bd5
宋英杰 <picnic106@163.com>
2015-08-18 15:00:21 +0800

    Update zj.js

--- js/cookie/zj.js
+++ js/cookie/zj.js
@@ -15,3 +15,12 @@
       return unescape(arr[2]);
     return null;
   }
+
+
+js如何设置一个全站都能使用的cookie来记住用户名，记住访客的信息，方便访客下次的操作。
+js设置一个cookie网上有很多资料，但是只能在同目录下使用，其他地方就失效了。设置全局cookie只需加一个参数即可。
+
+把 document.cookie=c_name+ "=" +escape(value)+
+((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
+改成 document.cookie=c_name+ "=" +escape(value)+
+(expiredays==null) ? "" : ";expires="+exdate.toGMTString())+"; path=/";
