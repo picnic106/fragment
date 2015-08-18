65ead37d9a6972b4348c32aa008d97415c86ff8a
宋英杰 <picnic106@163.com>
2015-08-18 14:57:41 +0800

    Create zj.js

---
+++ js/cookie/zj.js
@@ -0,0 +1,17 @@
+//写入cookie
+  function SetCookie(name,value)
+  {
+    var Days = 30; //此 cookie 将被保存 30 天
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days*24*60*60*1000);
+    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString()+";path=/";
+  }
+
+  //读取cookie
+  function getCookie(name)
+  {
+    var arr = document.cookie.match(new RegExp("(^| )"+name+"=([^;]*)(;|$)"));
+    if(arr != null)
+      return unescape(arr[2]);
+    return null;
+  }
